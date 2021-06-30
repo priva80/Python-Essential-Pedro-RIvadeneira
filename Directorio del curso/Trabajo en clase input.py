23e3eb2774e2a2d35281d9e37b89050b753593ff
priva80 <86371924+priva80@users.noreply.github.com>
2021-06-30 13:49:19 -0500

    Add files via upload

---
+++ Directorio del curso/Trabajo en clase input.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 25 18:30:24 2021
+
+@author: Envy-W10
+"""
+Nombre=input("Ingrese su nombre")
+Apellido=input("Ingrese su apellido")
+Edad=int (input("Ingrese su edad"))
+Ubicación=input("Ingrese su ubicación")
+Space=" "
+"\n"*2, print("Sus datos son los siguientes:","\n"*2, Nombre+Space+Apellido+Space+str(Edad)+Space+Ubicación)
+
