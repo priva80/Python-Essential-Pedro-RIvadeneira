9177feda9bf152c834978be2f01a5ad204e21323
priva80 <86371924+priva80@users.noreply.github.com>
2021-06-30 13:38:44 -0500

    Add files via upload

---
+++ Directorio del curso/Concatenación.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 24 18:55:46 2021
+
+@author: Usuario
+"""
+
+str1= "Juan"
+str2= "es"
+str3= "Alto"
+space=" "
+numb= 27
+
+pi=22/7
+
+print("\t",str1+space+str2+space+str3)
+print(str1,str2,str3,"\n"*(2))
+
+print(str1,str2,str3,str(numb))
+
+print(str1,"\t"*2,str2,str3)
+ 
+print(type(numb))
+
+print(pi)
+print("{:.2f}".format(pi))
+ 
+
